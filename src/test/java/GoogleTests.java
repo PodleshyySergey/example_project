dbf337cfba8d884a9d70fdf5d7168246cad2d6a1
PodleshyySergey <60898756+PodleshyySergey@users.noreply.github.com>
2021-01-20 17:40:05 +0300

    Add new build.gradle and add test searchTest().

--- src/test/java/GoogleTests.java
+++ src/test/java/GoogleTests.java
@@ -1,2 +1,18 @@
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
 public class GoogleTests {
+
+    @Test
+    public void searchTest() {
+
+        open("https://google.com");
+        $(byText("q")).setValue("selenide").pressEnter();
+        $("html").shouldHave(text("selenide.org"));
+    }
+
 }
